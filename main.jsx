03f1b08bc7182d35516a977a16e6d91ef474d937
shmuli <shmulikeller@gmail.com>
2024-03-31 16:22:21 -0400

    seperated-create-comment-form-into-component

--- main.jsx
+++ main.jsx
@@ -1,71 +1,92 @@
 const EmptyListMessage = () => {
-    return (
-            <h3 style={{ color: 'darkorange', padding: "8px" }}>This list is empty. would you like to add a task</h3>
-    )
-}
-
-
-function List({ listName, rowIndex, list, comments, setComments, dragEnter, dragEnd, entered, addTask }) {
-    const inputRef = React.useRef()
   return (
-    <div class={entered? "list entered": "list"}
-    onDragEnter={() => {
+    <h3 style={{ color: "darkorange", padding: "8px" }}>
+      This list is empty. would you like to add a task
+    </h3>
+  );
+};
+
+function List({
+  listName,
+  rowIndex,
+  list,
+  comments,
+  setComments,
+  dragEnter,
+  dragEnd,
+  entered,
+  addTask,
+}) {
+  const inputRef = React.useRef();
+  return (
+    <div
+      class={entered ? "list entered" : "list"}
+      onDragEnter={() => {
         dragEnter(rowIndex);
       }}
-     
-      onDragEnd={dragEnd}>
+      onDragEnd={dragEnd}
+    >
       <h1>
         #{rowIndex + 1}: {listName}
       </h1>
-      {list.length == 0?  <EmptyListMessage/>: list.map((item, colIndex) => (
-        <Task {...{ colIndex, rowIndex, item, setComments, comments }} />
-      ))}
+      {list.length == 0 ? (
+        <EmptyListMessage />
+      ) : (
+        list.map((item, colIndex) => (
+          <Task {...{ colIndex, rowIndex, item, setComments, comments }} />
+        ))
+      )}
       <form class="add-task-form">
         <input ref={inputRef} type="text" />
-      <button onClick={(e) => {
-        e.preventDefault()
-        const inputText = inputRef.current.value;
-        inputRef.current.value = "";
-        if (!inputText){
-            return
-        }
-        addTask(rowIndex, inputText)
-      }}>add task</button>
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            const inputText = inputRef.current.value;
+            inputRef.current.value = "";
+            if (!inputText) {
+              return;
+            }
+            addTask(rowIndex, inputText);
+          }}
+        >
+          add task
+        </button>
       </form>
     </div>
   );
 }
 
 function App() {
-  const [lists, setLists] = React.useState([
-    [],[],[]
-  ]);
-  const [comments, setComments] = React.useState([
-    [],[],[]
-  ]);
+  const [lists, setLists] = React.useState([[], [], []]);
+  const [comments, setComments] = React.useState([[], [], []]);
 
   let held, entered;
 
-  function deleteTask(rowIndex, colIndex){
-    const clonedLists = [...lists];
-    const clonedComments = [...comments];
-
-    delete clonedLists[rowIndex][colIndex]
-    delete clonedComments[rowIndex][colIndex]
-
-    setLists(clonedLists)
-    setComments(clonedComments)
+  function createComment(rowIndex, colIndex, input){
+        comments[rowIndex][colIndex].push(`${thisUser}: ${input}`);
+        setComments([...comments]);
   }
 
-  function addTask(rowIndex, inputText){
+  function deleteTask(rowIndex, colIndex) {
     const clonedLists = [...lists];
     const clonedComments = [...comments];
 
-    clonedLists[rowIndex].push(inputText)
-    clonedComments[rowIndex].push([])
+    delete clonedLists[rowIndex][colIndex];
+    delete clonedComments[rowIndex][colIndex];
 
-    setLists(clonedLists)
-    setComments(clonedComments)
+    setLists(clonedLists);
+    setComments(clonedComments);
+  }
+
+  function addTask(rowIndex, inputText) {
+    const clonedLists = [...lists];
+    const clonedComments = [...comments];
+
+    clonedLists[rowIndex].push(inputText);
+    clonedComments[rowIndex].push([]);
+
+    setLists(clonedLists);
+    setComments(clonedComments);
   }
 
   function dragStart(rowIndex, colIndex) {
@@ -73,49 +94,57 @@ function App() {
   }
 
   function dragEnter(rowIndex) {
-      
-      entered = rowIndex
+    entered = rowIndex;
   }
   function dragEnd() {
-    const [rowIndex, colIndex] = held
+    const [rowIndex, colIndex] = held;
 
     const clonedLists = [...lists];
     const clonedComments = [...comments];
-    
-    clonedLists[entered].push(lists[rowIndex][colIndex])
-    delete clonedLists[rowIndex][colIndex]
 
-    clonedComments[entered].push(comments[rowIndex][colIndex])
-    delete clonedComments[rowIndex][colIndex]
+    clonedLists[entered].push(lists[rowIndex][colIndex]);
+    delete clonedLists[rowIndex][colIndex];
 
-    setLists(clonedLists)
-    setComments(clonedComments)
+    clonedComments[entered].push(comments[rowIndex][colIndex]);
+    delete clonedComments[rowIndex][colIndex];
+
+    setLists(clonedLists);
+    setComments(clonedComments);
   }
 
   const listNames = ["todo", "doing", "done"];
   return (
     <main>
+        
+      <createCommentContext.Provider value={createComment}>
       <dragContext.Provider value={dragStart}>
-      <deleteTaskContext.Provider value={deleteTask}>
-        {listNames.map((listName, rowIndex) => (
-          <List
-            {...{ listName, rowIndex, comments, setComments, dragEnter, dragEnd, addTask }}
-            entered={entered == rowIndex}
-            list={lists[rowIndex]}
-          />
-        ))}
-      </deleteTaskContext.Provider>
-      </dragContext.Provider>
+        <deleteTaskContext.Provider value={deleteTask}>
+          {listNames.map((listName, rowIndex) => (
+            <List
+              {...{
+                listName,
+                rowIndex,
+                comments,
+                setComments,
+                dragEnter,
+                dragEnd,
+                addTask,
+              }}
+              entered={entered == rowIndex}
+              list={lists[rowIndex]}
+            />
+          ))}
+        </deleteTaskContext.Provider>
+        </dragContext.Provider>
+      </createCommentContext.Provider>
     </main>
   );
 }
 
-
 function Task({ rowIndex, colIndex, item, setComments, comments }) {
   const [showComments, setShowComments] = React.useState(true);
-  const commentInputRef = React.useRef();
-  const dragStart  = React.useContext(dragContext);
-  const deleteTask  = React.useContext(deleteTaskContext);
+  const dragStart = React.useContext(dragContext);
+  const deleteTask = React.useContext(deleteTaskContext);
 
   return (
     <div
@@ -123,45 +152,70 @@ function Task({ rowIndex, colIndex, item, setComments, comments }) {
       draggable
       onDragStart={() => dragStart(rowIndex, colIndex)}
     >
-        <button class="delete-task-button" onClick={() => {deleteTask(rowIndex, colIndex)}}>X</button>
-      <h3 class="issue-text">issue: {item}</h3>
-      <button onClick={() => setShowComments(!showComments)}>
-        {showComments ? "hide comments" : "show comments"}{" "}
+      <button
+        class="delete-task-button"
+        onClick={() => {
+          deleteTask(rowIndex, colIndex);
+        }}
+      >
+        X
       </button>
-      {showComments ? (
+      <h3 class="issue-text">{item}</h3>
+      <button onClick={() => setShowComments(!showComments)}>
+        {showComments? "hide comments" : `show (${comments[rowIndex][colIndex].length}) comments`}{" "}
+      </button>
+      {showComments? (
         <div class="comments">
-          {comments[rowIndex][colIndex].map((cmnt) => (
+          {comments[rowIndex][colIndex].slice().reverse().map((cmnt) => (
             <p>{cmnt}</p>
           ))}
         </div>
       ) : (
         ""
       )}
-      <form>
-        <input ref={commentInputRef} type="text" />
-        <button
-          type="submit"
-          onClick={(event) => {
-            event.preventDefault();
-            const input = commentInputRef.current.value;
-            if (!input) {
-                alert("no content provided as input")
-              return;
-            }
-            comments[rowIndex][colIndex].push(`${thisUser}: ${input}`);
-            commentInputRef.current.value = "";
-            setComments([...comments]);
-            setShowComments(true);
-          }}
-        >
-          comment
-        </button>
-      </form>
+      <AddCommentForm
+        {...{rowIndex, colIndex}}
+      />
     </div>
   );
 }
 
-const thisUser = "shmuli"
+
+
+function AddCommentForm({
+  rowIndex,
+  colIndex,
+}) {
+  const commentInputRef = React.useRef();
+  const createComment = React.useContext(createCommentContext);
+
+  return (
+    <form>
+      <input ref={commentInputRef} type="text" />
+      <button
+        type="submit"
+        onClick={(event) => {
+          event.preventDefault();
+          const input = commentInputRef.current.value;
+          commentInputRef.current.value = "";
+
+          if (!input) {
+            alert("no content provided as input");
+            return;
+          }
+
+          createComment(rowIndex, colIndex, input)
+          
+        }}
+      >
+        comment
+      </button>
+    </form>
+  );
+}
+
+const thisUser = "shmuli";
 const dragContext = React.createContext(null);
 const deleteTaskContext = React.createContext(null);
+const createCommentContext = React.createContext(null);
 ReactDOM.render(<App />, document.querySelector("#root"));
